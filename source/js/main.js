4c637dde21c8c556b96da89781a27b1f722023c2
Kat <ekatanti@yandex.ru>
2021-02-08 22:56:27 +0300

    Добавляет блоки: о нас, виды велосипедов и видео

--- source/js/main.js
+++ source/js/main.js
@@ -9,10 +9,21 @@ menu.classList.remove('main-nav--nojs');
 menuToggle.classList.remove('header__menu-button--nojs');
 logoWrapper.classList.remove('header__logo-wrapper--nojs');
 
-menuToggle.addEventListener('click', function () {
+var toggleMobileMenu = function () {
   menu.classList.toggle('main-nav--closed');
   logoWrapper.classList.toggle('header__logo-wrapper--open');
   menuToggle.classList.toggle('header__menu-button--open');
   menuToggle.classList.toggle('header__menu-button--close');
   body.classList.toggle('body--overflow');
+};
+
+menuToggle.addEventListener('click', function () {
+  toggleMobileMenu();
+});
+
+
+menu.addEventListener('click', function (evt) {
+  if (evt.target.matches('a') && !menu.classList.contains('main-nav--closed')) {
+    toggleMobileMenu();
+  }
 });
